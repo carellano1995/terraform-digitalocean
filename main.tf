a5c40b041a4e6f61e132d1f171b1ac526b4acd21
Cristian Arellano <cristian.arellano@23people.io>
2021-09-11 01:39:07 -0300

    add terraform base droplet

--- main.tf
+++ main.tf
@@ -29,6 +29,6 @@ resource "digitalocean_droplet" "web" {
 
 resource "digitalocean_ssh_key" "default" {
   name       = "Carellano"
-  public_key = "ssh-rsa ..."
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDA0OI37fIwElQMqZeoymVV+FkG3DbtP3Ch2FuC00DXEr3PYg0dY0McbehukOKRdivcCiIjDKa3iBio0hlE9IXqaes1KaTNsLIncBwfEkgfxh49X1jiVIPDXQWV4zVIeVwAsgS922L7K83OWFHcYMRZnnJ/zqC183+GPvjCmy8zavf0K6yngIn13/5gFtJXhdqORr8jeZXx6cINSY31c6bjFtkBY7gAtTIAkEgJLEzP6Nvi3D45FmeIDvXT5RY1IsIGi9FfEAJE7EzVxhMxYsjhAKsIAetvlh9vQEF6N4tX3bVNgnueEBggiFOP5QII+raGTCl7+H3qHz/dirDOkld1 carellano@Admins-MacBook-Pro.local"
 }
 
